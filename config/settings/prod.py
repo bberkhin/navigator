df8cb6adbec0d68b54a05eb379228f6bec3443d0
bberkhin <bberkhin@gmail.com>
2026-05-09 10:54:02 +0700

    убрал Postgress

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,15 +4,10 @@ import os
 DEBUG = False
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
-# PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
